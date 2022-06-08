1f36475de5057166d2c57b71576fde11ebbe4ac6
Mateo Contreras <contreras4893@gmail.com>
2022-06-08 10:35:38 -0600

    se modificó la condición para detectar obstáculos

--- ese.ino
+++ ese.ino
@@ -1,6 +1,6 @@
 #include "RobotCar.h"
 Carro car;
-int detener = 15;
+int detener = 10;
 int giro = 90;
 
 void setup(){
@@ -9,12 +9,37 @@ void setup(){
 }
 
 void loop(){
-    if (car.UltraC->medir() > detener)
+//    if (car.UltraC->medir() > detener)
+//    {
+//        car.mover(10,10);
+//    }else{
+//        car.mover(0,0);
+//        delay(100);
+//        car.UltraI->medir() > car.UltraD->medir() ? car.girar(giro) : car.girar(-1*giro);
+//    }
+
+    if (car.UltraC->medir() < detener)
     {
-        car.mover(10,10);
-    }else{
         car.mover(0,0);
         delay(100);
         car.UltraI->medir() > car.UltraD->medir() ? car.girar(giro) : car.girar(-1*giro);
     }
+
+    if (car.UltraI->medir() < detener){
+        car.mover(0,0);
+        delay(100);
+        car.girar(giro);
+    }
+    if (car.UltraD->medir() < detener){
+        car.mover(0,0);
+        delay(100);
+        car.girar(-1*giro);
+    }
+
+    car.mover(10,10);
+
+    Serial.print("rueda izqierda: ");
+    Serial.println(car.contaI);
+    Serial.print("rueda derecha: ");
+    Serial.println(car.contaD);
 }
